0a55c51f8138ef25f34ccd84914277179dc673e9
affanrana612 <affanrana612@gmail.com>
2026-07-02 15:07:39 +0500

    git push

--- day_11/app.py
+++ day_11/app.py
@@ -50,3 +50,54 @@
 #     print("my name is" + "      " + fname +"      ", mname , "    " + lname)
 # fname("Abdul", "Wahhab", "Rana")
 
+# def sum():
+#     return"hello"  # get back value
+# againsum = sum ()
+# print(againsum)
+
+
+# a = 2  # assign 
+# a == 2 # equal to
+# 2 === 2  # the triple = is variable in number #   check data types  # in javascript
+
+
+# -- topic: (data & interactivity)
+# data - information
+# which use for input and output process
+# which for input and output process
+# name = "Abdul Wahhab Rana"   #-string (str)
+# age = 14    #-integer (int)
+# height = 5.1    #-float (float)
+# isactive = True   #-boolean (bool)
+ 
+# sum = "2" + "3"  #concatenation
+# print(sum)   
+
+# sum = 2 + "3"   #error
+# print(sum)
+
+# sum = 2 + 3.5     #temporary(coercion)
+# print(sum)
+
+# total = 300
+# obtain = 100
+# percentage = obtain / total * 100
+# print(percentage)
+
+# - -- interactivity
+# input()
+# num1 = 3
+# num2 = 4
+# sum = num1 + num2
+# print(type(sum))
+
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# sum = int(num1) + int(num2)
+# print(sum)
+
+obtain = int(input("Enter obtain marks: "))
+total = int(input("Enter total marks: "))
+percentage = print("your percentage = ",obtain / total * 100,) 
+print(percentage)
