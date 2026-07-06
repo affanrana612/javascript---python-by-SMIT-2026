a81ae0c75ae8be3c8cc9ba4c2b1982ac259002c4
affanrana612 <affanrana612@gmail.com>
2026-07-06 15:59:05 +0500

    javascript connected

---
+++ day_13/app.js
@@ -0,0 +1,12 @@
+// for terminal app.js
+console.log("Hello JS ")
+console.log("HI JS 2 ")
+// alert("hello alert")
+
+// prompt("enter your name")
+
+
+// for browser js
+// create a HTml file
+// connect JS with Html
+// check is in browser  (ctrl + shif+ i) or (f12) or (right click on inspect)
