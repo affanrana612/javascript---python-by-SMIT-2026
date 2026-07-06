bae868eef437ab6df5ad3dd0f253053f5cd0c9df
affanrana612 <affanrana612@gmail.com>
2026-07-06 16:10:26 +0500

    updated app.js

--- day_13/app.js
+++ day_13/app.js
@@ -1,6 +1,6 @@
 // for terminal app.js
-console.log("Hello JS ")
-console.log("HI JS 2 ")
+///console.log("Hello JS ")
+// console.log("HI JS 2 ")
 // alert("hello alert")
 
 // prompt("enter your name")
@@ -10,3 +10,21 @@ console.log("HI JS 2 ")
 // create a HTml file
 // connect JS with Html
 // check is in browser  (ctrl + shif+ i) or (f12) or (right click on inspect)
+
+
+// name = "ALI"
+// console.log(name)
+
+// key word for variable
+
+// var name = "ALI" - can update and redeclare
+// let name = "ALI"  - can update but not redeclare
+// const name = "ALI" - can not update or redeclare
+
+// var myname = "ALI"
+// myname= "AFFAN"
+// console.log(myname)
+
+const myname = "ALI"
+const myname= "AFFAN"
+console.log(myname)
