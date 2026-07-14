d71f1fe9ffb2ef2f07c0fa3b0c131f9c2bcc5711
affanrana612 <affanrana612@gmail.com>
2026-07-14 14:37:10 +0500

    added main.p and script.js

---
+++ day_15/main.py
@@ -0,0 +1,32 @@
+# function
+# def is  keyword
+# function name
+# () round bracket
+# : colon
+
+# argument & parameter
+# def calculate(j):
+#     print(j)
+#     # a = 9
+#     # b=7
+
+#     # c= a+b
+#     # d = c/10
+#     # print(d)
+    
+# calculate(8)
+# calculate()
+# calculate()
+# calculate()
+
+ 
+#  number of times repeat krna ho
+
+# For key word
+# variable name
+# in 
+# range()
+
+for i in range(1,10):
+    # code
+    print("hello")
---
+++ day_15/script.js
@@ -0,0 +1,11 @@
+//  make function
+
+
+// function calculate(){
+//     console.log("hello")
+// }
+// calculate()
+
+for(var i=0; i<9;i++){
+    console.log("hello")
+}
